3cbccf4f85efba71e62a00c98cedadb00015cbad
Kong To <newlight77@gmail.com>
2020-03-16 23:53:35 +0100

    displaying test results summary

--- build.gradle.kts
+++ build.gradle.kts
@@ -81,4 +81,42 @@ tasks.withType<Test> {
         showStackTraces = true
         showStandardStreams = true
     }
+
+    val failedTests = mutableListOf<TestDescriptor>()
+    val skippedTests = mutableListOf<TestDescriptor>()
+
+    // See https://technology.lastminute.com/junit5-kotlin-and-gradle-dsl/
+    // See https://github.com/gradle/kotlin-dsl/issues/836
+    addTestListener(object : TestListener {
+        override fun beforeSuite(suite: TestDescriptor) {}
+        override fun beforeTest(testDescriptor: TestDescriptor) {}
+        override fun afterTest(testDescriptor: TestDescriptor, result: TestResult) {
+            when (result.resultType) {
+                TestResult.ResultType.FAILURE -> failedTests.add(testDescriptor)
+                TestResult.ResultType.SKIPPED -> skippedTests.add(testDescriptor)
+                else -> Unit
+            }
+        }
+
+        override fun afterSuite(suite: TestDescriptor, result: TestResult) {
+            if (suite.parent == null) { // root suite
+                logger.lifecycle("----")
+                logger.lifecycle("Test result: ${result.resultType}")
+                logger.lifecycle(
+                        "Test summary: ${result.testCount} tests, " +
+                        "${result.successfulTestCount} succeeded, " +
+                        "${result.failedTestCount} failed, " +
+                        "${result.skippedTestCount} skipped")
+                failedTests.takeIf { it.isNotEmpty() }?.prefixedSummary("\tFailed Tests")
+                skippedTests.takeIf { it.isNotEmpty() }?.prefixedSummary("\tSkipped Tests:")
+            }
+        }
+
+        private infix fun List<TestDescriptor>.prefixedSummary(subject: String) {
+                logger.lifecycle(subject)
+                forEach { test -> logger.lifecycle("\t\t${test.displayName()}") }
+        }
+
+        private fun TestDescriptor.displayName() = parent?.let { "${it.name} - $name" } ?: "$name"
+    })
 }
